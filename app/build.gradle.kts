8ed55b2a166c3dba77787aa8601faf8de91ea0e3
gawwy23-lgtm <gawwy23@googlemail.com>
2025-10-05 00:23:47 +0200

    Create build.gradle.kts

---
+++ app/build.gradle.kts
@@ -0,0 +1,7 @@
+app/build.gradle.kts
+app/src/main/AndroidManifest.xml
+app/src/main/java/de/famopt/optimizer/MainActivity.kt
+app/src/main/java/de/famopt/optimizer/Tax.kt
+app/src/main/res/values/themes.xml
+app/src/main/res/mipmap-anydpi-v26/ic_launcher.xml
+app/src/main/assets/luenen_kita_2024.json
